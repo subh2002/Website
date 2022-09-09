c41ff4af28ef6c51e4e85067b43535c298fd595b
Subhash Jha <subhashjha18@hotmail.com>
2022-09-09 11:32:34 +0530

    forms Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -292,3 +292,27 @@
   });
 
 })()
+
+ /**
+   * Send Mail Function
+   */
+  // var btn = document.getElementById('submit');
+  // btn.addEventListener('click',function(e) {
+  //     e.preventDefault()
+  //     var name = document.getElementById('name').value;
+  //     var email = document.getElementById('email').value;
+  //     var subject = document.getElementById('subject').value;
+  //     var message = document.getElementById('message').value;
+  //     var body = 'name:' +name + '<br/>email: ' +email + '<br/>subject: ' +subject + '<br/>message: ' +message;
+  //    Email.send({
+  //   Host : "smtp.gmail.com",
+  //   Username : "contact.ingotec@gmail.com",
+  //   Password : "kkvxtqshvdbwavux",
+  //   To : 'contact.ingotec@gmail.com',
+  //   From : email,
+  //   Subject : subject,
+  //   Body : body
+  // }).then(
+  //   message => alert(message)
+  // );
+  // })
